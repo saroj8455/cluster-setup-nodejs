91cde18f8d50c654768084f8e4a64cc509fa5606
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-08-03 23:58:59 +0530

    implement ip rate limit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import { rateLimit } from 'express-rate-limit';
 
 const port = 3000;
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: {
+    error: 'Too many requests, please try again after 15 minutes',
+    status: false,
+  },
+});
+
+app.use(limiter);
 
 app.get('/heavy', (req, res) => {
   let total = 0;
@@ -13,6 +25,15 @@ app.get('/heavy', (req, res) => {
   res.jsonp({
     message: 'Heavy computation done',
     total,
+  });
+});
+
+app.get('/api/v1', (req, res) => {
+  return res.status(200).jsonp({
+    id: 1,
+    tech: 'nodejs',
+    description:
+      'Rate limiting is a strategy for limiting network traffic. It puts a cap on how often someone can repeat an action within a certain timeframe – for instance, trying to log in to an account. Rate limiting can help stop certain kinds of malicious bot activity. It can also reduce strain on web servers. However, rate limiting is not a complete solution for managing bot activity.',
   });
 });
 
